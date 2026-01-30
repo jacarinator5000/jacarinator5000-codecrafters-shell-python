bb35246ca874546d4f5efef53d69db1ca9cecf51
jeva740 <jeva740@wgu.edu>
2026-01-29 22:47:57 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,6 +5,9 @@ def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
     pass
+    # Wait for user input
+    command = input()
+    print(f"[command]: command not found")
 
 
 if __name__ == "__main__":
