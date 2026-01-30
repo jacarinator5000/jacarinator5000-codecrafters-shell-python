0fefd042aa427726bb60cdc4b379a0de389a1082
jeva740 <jeva740@wgu.edu>
2026-01-29 23:20:07 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,11 +4,14 @@ import sys
 def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
-    pass
-    # Wait for user input
+    sys.stdout.flush()
+    
+    # read command from stdin
     command = input()
-    print(f"{command}: command not found")
-
+     
+    # print error message for invalid command
+    sys.stdout.write(f"{command}: command not found\n")
+    main()
 
 if __name__ == "__main__":
     main()
