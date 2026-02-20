7815f941c17079ec9f13e7885b4bc0c39ea35122
jeva740 <jeva740@wgu.edu>
2026-02-19 20:16:28 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,20 +3,31 @@ import sys
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
-    
-    # read command from stdin
-    command = input()
-    
-    # command for exiting shell
-    if command.upper() == "EXIT":
-        sys.exit()
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
+
+        # read command from stdin
+        line = input().strip()
+        if not line:
+            continue
         
-     
-    # print error message for invalid command
-    sys.stdout.write(f"{command}: command not found\n")
-    main()
+        parts = line.split()
+        command = parts[0].lower()
+        args = parts[1:]
+
+        # command for exiting shell
+        if command == "exit":
+            sys.exit()
+
+        # print arguments to stdout
+        elif command == "echo":
+            sys.stdout.write("".join(args) + "\n")
+
+        # print error message for invalid command
+        else:
+            sys.stdout.write(f"{command}: command not found\n")    
+    
 
 if __name__ == "__main__":
     main()
